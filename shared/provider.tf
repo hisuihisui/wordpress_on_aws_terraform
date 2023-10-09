819aa486740e4a0ac1479f181857c15117181186
hisuihisui <ruru.apple.1212@gmail.com>
2023-10-10 00:16:04 +0900

    modify_backend

--- shared/provider.tf
+++ shared/provider.tf
@@ -7,15 +7,6 @@ terraform {
   }
 
   required_version = ">= 1.6.0"
-
-  # tfstateの保存先
-  backend "s3" {
-    bucket = "tfstate-animevoyager"
-    region = "ap-northeast-1"
-    # バケット内の保存先
-    key = "tfstate/terraform.tfstate"
-    encrypt = true
-  }
 }
 
 provider "aws" {
