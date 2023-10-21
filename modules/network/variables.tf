8166ce903ded6ebbde52030dabc92b0aaaab6cde
hisuihisui <ruru.apple.1212@gmail.com>
2023-10-21 19:12:01 +0900

    delete network module

--- modules/network/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "cidr_block" {
-  default = "172.16.0.0/16"
-}
-
-
-variable "prefix" {
-  type = string
-}
--- services/animevoyager/main.tf
+++ services/animevoyager/main.tf
@@ -1,5 +1,39 @@
-# Network
-module "network" {
-  source = "../../modules/network"
-  prefix = local.prefix
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr_block
+
+  tags = {
+    Name = "${local.prefix}-vpc"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.prefix}-internet_gateway"
+  }
+}
+
+
+# Subnet
+module "subnet_1a" {
+  source              = "../subnet"
+  count_nat_gateway   = 1
+  internet_gateway_id = aws_internet_gateway.public.id
+  prefix              = local.prefix
+  vpc_id              = aws_vpc.main.id
+  # 他はデフォルト値でOK
+}
+
+module "subnet_1c" {
+  source              = "../subnet"
+  az                  = "ap-northeast-1c"
+  count_nat_gateway   = 0
+  internet_gateway_id = aws_internet_gateway.public.id
+  prefix              = local.prefix
+  private_subnet_cidr = "172.16.11.0/24"
+  public_subnet_cidr  = "172.16.10.0/24"
+  vpc_id              = aws_vpc.main.id
 }
