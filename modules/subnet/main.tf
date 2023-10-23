a60f14d161447f772d0e51a64600365549d1bb7c
hisuihisui <ruru.apple.1212@gmail.com>
2023-10-23 23:04:57 +0900

    ec2 move to public subnet

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -37,63 +37,63 @@ resource "aws_route_table_association" "public" {
 
 
 # Private Subnet
-resource "aws_subnet" "private" {
-  vpc_id            = var.vpc_id
-  cidr_block        = var.private_subnet_cidr
-  availability_zone = var.az
-  # パブリックIPアドレス不要なので、false
-  map_public_ip_on_launch = false
+# resource "aws_subnet" "private" {
+#   vpc_id            = var.vpc_id
+#   cidr_block        = var.private_subnet_cidr
+#   availability_zone = var.az
+#   # パブリックIPアドレス不要なので、false
+#   map_public_ip_on_launch = false
 
-  tags = {
-    Name = "${var.prefix}-private_subnet-${var.az}"
-  }
-}
+#   tags = {
+#     Name = "${var.prefix}-private_subnet-${var.az}"
+#   }
+# }
 
 # Route Table
 # Private Subnetが使用
-resource "aws_route_table" "private" {
-  vpc_id = var.vpc_id
+# resource "aws_route_table" "private" {
+#   vpc_id = var.vpc_id
 
-  tags = {
-    Name = "${var.prefix}-private-route_table-${var.az}"
-  }
-}
+#   tags = {
+#     Name = "${var.prefix}-private-route_table-${var.az}"
+#   }
+# }
 
-# ルートテーブルとプライベートサブネットを関連付け
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
-}
+# # ルートテーブルとプライベートサブネットを関連付け
+# resource "aws_route_table_association" "private" {
+#   subnet_id      = aws_subnet.private.id
+#   route_table_id = aws_route_table.private.id
+# }
 
 # Route
 # プライベートサブネット → NATゲートウェイへの通信を許可
-resource "aws_route" "private" {
-  # リソースを作成するか
-  # 0：作成しない、1：作成する
-  count                  = var.count_nat_gateway
-  route_table_id         = aws_route_table.private.id
-  nat_gateway_id         = aws_nat_gateway.main[0].id
-  destination_cidr_block = "0.0.0.0/0"
-}
+# resource "aws_route" "private" {
+#   # リソースを作成するか
+#   # 0：作成しない、1：作成する
+#   count                  = var.count_nat_gateway
+#   route_table_id         = aws_route_table.private.id
+#   nat_gateway_id         = aws_nat_gateway.main[0].id
+#   destination_cidr_block = "0.0.0.0/0"
+# }
 
-# NATゲートウェイ用EIP
-resource "aws_eip" "nat_gateway" {
-  count  = var.count_nat_gateway
+# EC2用EIP
+resource "aws_eip" "ec2" {
+  count  = var.count_ec2
   domain = "vpc"
   tags = {
-    Name = "${var.prefix}-eip-nat_gateway-${var.az}"
+    Name = "${var.prefix}-eip-ec2-${var.az}"
   }
 }
 
-# NATゲートウェイ
-resource "aws_nat_gateway" "main" {
-  count = var.count_nat_gateway
-  # EIPの指定
-  allocation_id = aws_eip.nat_gateway[0].id
-  # サブネット
-  # パブリックサブネットを指定すること
-  subnet_id = aws_subnet.public.id
-  tags = {
-    Name = "${var.prefix}-nat_gateway-${var.az}"
-  }
-}
+# # NATゲートウェイ
+# resource "aws_nat_gateway" "main" {
+#   count = var.count_nat_gateway
+#   # EIPの指定
+#   allocation_id = aws_eip.nat_gateway[0].id
+#   # サブネット
+#   # パブリックサブネットを指定すること
+#   subnet_id = aws_subnet.public.id
+#   tags = {
+#     Name = "${var.prefix}-nat_gateway-${var.az}"
+#   }
+# }
--- services/animevoyager/main.tf
+++ services/animevoyager/main.tf
@@ -20,7 +20,8 @@ resource "aws_internet_gateway" "public" {
 # Subnet
 module "subnet_1a" {
   source              = "../../modules/subnet"
-  count_nat_gateway   = 1
+  count_ec2           = 1
+  instance_id         = module.ec2_1a.instance_id
   internet_gateway_id = aws_internet_gateway.public.id
   prefix              = local.prefix
   vpc_id              = aws_vpc.main.id
@@ -30,7 +31,7 @@ module "subnet_1a" {
 module "subnet_1c" {
   source              = "../../modules/subnet"
   az                  = "ap-northeast-1c"
-  count_nat_gateway   = 0
+  count_ec2           = 0
   internet_gateway_id = aws_internet_gateway.public.id
   prefix              = local.prefix
   private_subnet_cidr = "172.16.11.0/24"
@@ -41,10 +42,10 @@ module "subnet_1c" {
 
 # EC2
 module "ec2_1a" {
-  source            = "../../modules/ec2"
-  ami               = local.ami
-  instance_type     = local.instance_type
-  prefix            = local.prefix
-  private_subnet_id = module.subnet_1a.private_subnet_id
-  vpc_id            = aws_vpc.main.id
+  source           = "../../modules/ec2"
+  ami              = local.ami
+  instance_type    = local.instance_type
+  prefix           = local.prefix
+  public_subnet_id = module.subnet_1a.public_subnet_id
+  vpc_id           = aws_vpc.main.id
 }
