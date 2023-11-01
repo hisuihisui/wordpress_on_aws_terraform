b0ddeec8692f18a67bc1f37a39b89d4cb29b50c6
hisuihisui <ruru.apple.1212@gmail.com>
2023-11-02 00:20:30 +0900

    ssl_policyの修正

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -68,7 +68,7 @@ resource "aws_lb_listener" "wordpress_alb_https" {
   load_balancer_arn = aws_lb.wordpress_alb.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = var.certificate_arn
 
   default_action {
