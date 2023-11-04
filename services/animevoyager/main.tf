9164c01ea9bc9a4ef027c83ca4c36de85f0d6361
hisuihisui <ruru.apple.1212@gmail.com>
2023-11-05 00:07:26 +0900

    module名変更

--- services/animevoyager/main.tf
+++ services/animevoyager/main.tf
@@ -18,16 +18,16 @@ resource "aws_internet_gateway" "public" {
 
 
 # Subnet
-module "subnet_1a" {
-  source              = "../../modules/subnet"
+module "network_1a" {
+  source              = "../../modules/network"
   internet_gateway_id = aws_internet_gateway.public.id
   prefix              = local.prefix
   vpc_id              = aws_vpc.main.id
   # 他はデフォルト値でOK
 }
 
-module "subnet_1c" {
-  source              = "../../modules/subnet"
+module "network_1c" {
+  source              = "../../modules/network"
   az                  = "ap-northeast-1c"
   internet_gateway_id = aws_internet_gateway.public.id
   prefix              = local.prefix
@@ -37,36 +37,36 @@ module "subnet_1c" {
 
 
 # EC2
-module "ec2_1a" {
-  source           = "../../modules/ec2"
-  alb_sg_id        = module.alb.alb_sg_id
+module "ec2_wordpress_1a" {
+  source           = "../../modules/ec2_wordpress"
+  alb_sg_id        = module.alb_external.alb_sg_id
   ami              = local.ami
   instance_type    = local.instance_type
   prefix           = local.prefix
-  public_subnet_id = module.subnet_1a.public_subnet_id
+  public_subnet_id = module.network_1a.public_subnet_id
   vpc_id           = aws_vpc.main.id
 }
 
 # ALB
-module "alb" {
-  source          = "../../modules/alb"
-  certificate_arn = module.route53.certificate_arn
+module "alb_external" {
+  source          = "../../modules/alb_external"
+  certificate_arn = module.domain.certificate_arn
   prefix          = local.prefix
   subnets = [
-    module.subnet_1a.public_subnet_id,
-    module.subnet_1c.public_subnet_id
+    module.network_1a.public_subnet_id,
+    module.network_1c.public_subnet_id
   ]
   vpc_id                = aws_vpc.main.id
   vpc_cidr_block        = local.vpc_cidr_block
-  wordpress_ec2_id_list = [module.ec2_1a.instance_id]
+  wordpress_ec2_id_list = [module.ec2_wordpress_1a.instance_id]
 }
 
 # Route53
-module "route53" {
+module "domain" {
   source = "../../modules/route53"
   domain = local.domain
   alb_parameter = {
-    dns_name = module.alb.alb_dns_name
-    zone_id  = module.alb.alb_zone_id
+    dns_name = module.alb_external.alb_dns_name
+    zone_id  = module.alb_external.alb_zone_id
   }
 }
